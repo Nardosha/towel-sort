47db87dff73b88a683958359fd8fb2af27726c60
Natosha <lalabumlala@gmail.com>
2022-02-23 23:01:43 +0300

    fix: add check

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@
 
 module.exports = function towelSort (matrix) {
     let result = []
+    if (matrix) {
+        matrix.map((item, index) => {
+            if (index % 2 !== 0) {
+                item.reverse()
+            }
+            for (let el of item) {
+                result.push(el)
+            }
 
-    matrix.map((item, index) => {
-        if (index % 2 !== 0) {
-            item.reverse()
-        }
-        for (let el of item) {
-            result.push(el)
-        }
-
-    })
+        })
+    }
     return result
 }
